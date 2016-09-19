c30c8e0774e5fcf79e4aeb8dbcf3eec3855a7e2e
vstudioss <chriswheels@visionaries.studio>
2016-09-20 00:10:40 +0100

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -16,8 +16,8 @@
 
 define('WP_REDIS_HOST', 'wordpress-001.c4yvp2.0001.usw1.cache.amazonaws.com');
  
-define('WP_HOME','http://wordpress-2052553139.us-west-1.elb.amazonaws.com/');
-define('WP_SITEURL','http://wordpress-2052553139.us-west-1.elb.amazonaws.com/');
+define('WP_HOME','dobog0tnx07ai.cloudfront.net');
+define('WP_SITEURL','dobog0tnx07ai.cloudfront.net');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
